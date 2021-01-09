449f61296589cd9889dbd8f07e9ce70a4d111dd0
Piyush Anand <piyushanand.1221@gmail.com>
2021-01-09 21:12:45 +0530

    project ready

--- server.js
+++ server.js
@@ -1,9 +1,43 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
+const ShortUrls = require("./models/shortUrl");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hi from Server");
+mongoose.connect(
+  "mongodb+srv://url_shortener:url_shortener@cluster0.hiv9g.mongodb.net/url_shortener?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: true,
+  }
+);
+
+app.set("view engine", "ejs");
+app.set("views", "src/views");
+
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", async (req, res) => {
+  const shortUrls = await ShortUrls.find();
+  res.render("index", { shortUrls: shortUrls });
+});
+
+app.post("/shortUrls", async (req, res) => {
+  await ShortUrls.create({ full: req.body.fullUrl });
+  res.redirect("/");
+});
+
+app.get("/:shortUrl", async (req, res) => {
+  const shortUrl = await ShortUrls.findOne({ short: req.params.shortUrl });
+  if (shortUrl == null) return res.sendStatus(404);
+
+  shortUrl.clicks++;
+  shortUrl.save();
+
+  res.redirect(shortUrl.full);
 });
 
 const port = process.env.PORT || 3000;
